2b686cc33b76c1b49de2ee1ecb9ea66faf1e1e48
Nasimul-Rahman <nasimul@cynergon.net>
2023-05-23 12:16:24 +0400

    added functionality

--- script.js
+++ script.js
@@ -16,20 +16,61 @@ const supportFormValue = document.getElementById('support_form_value');
 const midFormValue = document.getElementById('mid_form_value');
 const jungleFormValue = document.getElementById('jungle_form_value');
 
+function removeFromArray(index, userInputArrayModified, userInputArrayModifiedV1) {
+    for(let i = 0; i < userInputArrayModified.length; i++) {
+        if (i != index) {
+            userInputArrayModifiedV1.push(userInputArrayModified[i]);
+        }
+    }
+}
+
 document.getElementById('myForm').addEventListener('submit', function(event){
     event.preventDefault();
 
     let userInput = document.getElementById('name').value;
     let userInputArray = userInput.split(' ');
     let userInputArrayModified = [];
+    let userInputArrayModifiedV1 = []
 
     for(let i = 0; i < userInputArray.length; i++) {
         userInputArrayModified.push(userInputArray[i].replace(',', ''));
     }
 
-    topFormValue.innerHTML = "<h2>" + userInputArrayModified[0] + "</h2>";
-    bottomFormValue.innerHTML = "<h2>" + userInputArrayModified[1] + "</h2>";
-    supportFormValue.innerHTML = "<h2>" + userInputArrayModified[2] + "</h2>";
-    midFormValue.innerHTML = "<h2>" + userInputArrayModified[3] + "</h2>";
-    jungleFormValue.innerHTML = "<h2>" + userInputArrayModified[4] + "</h2>";
+    let topIndex = Math.floor(Math.random() * 5);
+    topFormValue.innerHTML = "<h2>" + userInputArrayModified[topIndex] + "</h2>";
+    removeFromArray(topIndex, userInputArrayModified, userInputArrayModifiedV1);
+
+    userInputArrayModified = userInputArrayModifiedV1
+    userInputArrayModifiedV1 = []
+
+    console.log(userInputArrayModified);
+
+    let bottomIndex = Math.floor(Math.random() * 4);
+    bottomFormValue.innerHTML = "<h2>" + userInputArrayModified[bottomIndex] + "</h2>";
+    removeFromArray(bottomIndex, userInputArrayModified, userInputArrayModifiedV1);
+
+    userInputArrayModified = userInputArrayModifiedV1
+    userInputArrayModifiedV1 = []
+
+    console.log(userInputArrayModified);
+
+    let supportIndex = Math.floor(Math.random() * 3);
+    supportFormValue.innerHTML = "<h2>" + userInputArrayModified[supportIndex] + "</h2>";
+    removeFromArray(supportIndex, userInputArrayModified, userInputArrayModifiedV1);
+
+    userInputArrayModified = userInputArrayModifiedV1
+    userInputArrayModifiedV1 = []
+
+    console.log(userInputArrayModified);
+
+    let midIndex = Math.floor(Math.random() * 2);
+    midFormValue.innerHTML = "<h2>" + userInputArrayModified[midIndex] + "</h2>";
+    removeFromArray(midIndex, userInputArrayModified, userInputArrayModifiedV1);
+
+    userInputArrayModified = userInputArrayModifiedV1
+    userInputArrayModifiedV1 = []
+
+    console.log(userInputArrayModified);
+
+    jungleFormValue.innerHTML = "<h2>" + userInputArrayModified[0] + "</h2>";
 })
